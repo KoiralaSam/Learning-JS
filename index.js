903be1b53aaa003da650674de94dfaeffc9d36b0
Samarpan Koirala <samarpankoirala@gmail.com>
2024-09-23 01:56:48 -0500

    Dice

--- index.js
+++ index.js
@@ -211,21 +211,38 @@ console.log(students);*/
 //     }
 // }
 
-const food1 = "Hamburger";
- const food2 = "Pizza";
- const food3 = "Sushi";
- const food4 = "hotdog";
- const food5 = "ramen";
+// const food1 = "Hamburger";
+//  const food2 = "Pizza";
+//  const food3 = "Sushi";
+//  const food4 = "hotdog";
+//  const food5 = "ramen";
 
 
-function getFoods(...foods){
-   return foods;
-}
+// function getFoods(...foods){
+//    return foods;
+// }
 
- function openFridge(...foods){
-    console.log(...foods);
+//  function openFridge(...foods){
+//     console.log(...foods);
+//  }
+
+//  const foods = getFoods(food1, food2, food3, food4, food5);
+
+//  openFridge(food1, food2, food3, food4, food5);
+function rollDice(){
+
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values = [];
+    const images = [];
+
+    for(let i = 0; i<numOfDice; i++){
+        const ranVal = Math.floor(Math.random() * 6 + 1);
+        values.push(ranVal);
+        images.push(`<img src = "/${ranVal}.png">`);
+    }
+
+    diceResult.textContent = `dice: ${values.join(", ")}`;
+    diceImages.innerHTML  = images.join('');
  }
-
- const foods = getFoods(food1, food2, food3, food4, food5);
-
- openFridge(food1, food2, food3, food4, food5);
