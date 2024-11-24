c55d290b11d8ea64f09144ce97fc18aac7d9964d
Samarpan Koirala <samarpankoirala@gmail.com>
2024-11-24 15:54:31 -0600

    hide and show

--- index.js
+++ index.js
@@ -1274,30 +1274,46 @@ console.log(students);*/
 // })
 
 //KeyEvents
-const mybox = document.getElementById("mybox");
-const moveAmount = 100;
-let x  = 0;
-let y = 0;
-document.addEventListener("keydown", event =>{
-    if(event.key.startsWith("Arrow")){
-        switch(event.key){
-            case "ArrowUp":
-                y -= moveAmount;
-                break;
+// const mybox = document.getElementById("mybox");
+// const moveAmount = 100;
+// let x  = 0;
+// let y = 0;
+// document.addEventListener("keydown", event =>{
+//     if(event.key.startsWith("Arrow")){
+//         switch(event.key){
+//             case "ArrowUp":
+//                 y -= moveAmount;
+//                 break;
             
-            case "ArrowDown":
-                y += moveAmount;
-                break;
-            case "ArrowLeft":
-                x -= moveAmount;
-                break;
-            case "ArrowRight":
-                x += moveAmount;
-                break;
-        }
+//             case "ArrowDown":
+//                 y += moveAmount;
+//                 break;
+//             case "ArrowLeft":
+//                 x -= moveAmount;
+//                 break;
+//             case "ArrowRight":
+//                 x += moveAmount;
+//                 break;
+//         }
 
-        mybox.style.top = `${y}px`;
-        mybox.style.left = `${x}px`;
+//         mybox.style.top = `${y}px`;
+//         mybox.style.left = `${x}px`;
 
+//     }
+// });
+
+//Hide/Show html elements
+const myButton = document.getElementById("myButton");
+const myImg = document.getElementById("myImg");
+console.log(myImg);
+myButton.addEventListener("click", event => {
+    if(myImg.style.visibility == "hidden"){
+        myImg.style.visibility = "visible";
+        myButton.textContent = "Hide";
     }
+    else{
+        myImg.style.visibility = "hidden";
+        myButton.textContent = "Show";
+    }
+    
 });
